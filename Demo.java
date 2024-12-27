cd50f4938087744ffbf3046351a519f89b5c9c61
dave2387 <davedhruv863@gmail.com>
2024-12-27 11:27:26 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+public static void main(String[] args){
+	
+	int a=Integer.parseInt(args[0]);
+	System.out.println(args[0]);
+	System.out.println(args[1]);
+	System.out.println(a+2);
+	
+	}
+}
---
+++ scan.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class scan{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println(a);
+
+	Double b=sc.nextDouble();
+	System.out.println(b);
+
+	String e=sc.nextLine();
+	String c=sc.nextLine();
+	System.out.println(c);
+
+	String d=sc.next();
+	System.out.println(d);
+	}
+}
+
+
+
+
+
+	
+	
