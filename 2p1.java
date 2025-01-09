6347799d82a16c917918dbbdadaea33c192e8723
dave2387 <davedhruv863@gmail.com>
2025-01-09 21:53:28 +0530

    Add files via upload

---
+++ 2p1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class p1{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+
+int a=10+20*30;
+System.out.println(a);
+
+int b=100/10*100;
+System.out.println(b);
+
+int c=5*4/4%3;
+System.out.println(c);
+
+int d=100+200/10-3*10;
+System.out.println(d);
+}
+}
---
+++ 2p4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class p4{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the f");
+		int f=sc.nextInt();
+		int c=f-32*5/9;
+		System.out.println(c);
+	}
+}
